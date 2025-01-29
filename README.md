669cad65b6bd4c3908c3564960b8f23c44aca680
AH mac <austinhart.ut@gmail.com>
2025-01-29 12:40:04 -0500

    mat up

---
+++ README.md
@@ -0,0 +1,11 @@
+# January 29: Wrangling (variables)
+
+The bulk of work on a typical data project is tidying (or wrangling, cleaning, munging): preparing the data for analysis. We'll spend two weeks on the topic. Our focus this week is on variables: creating, changing, renaming, reordering, and labeling. We'll learn that we can do a huge amount of this work within `mutate`. 
+
+## Useful resources
+- [Jenny Bryan's Chs 10-13](https://stat545.com/)
+- [Hadley Wickham's Ch 15](https://r4ds.had.co.nz/factors.html) is quite good on factors
+
+## Materials for class
+- Data: `water.csv`, DHS data from a region of Zimbabwe, children 6-15.
+- Materials: an R script for reference (I'll hand out a hard copy)
---
+++ variables.R
@@ -0,0 +1,153 @@
+#+ Wrangling: tidy variables
+#+ Jan 29, 2025
+
+# Setup ----------------
+# Packages
+  library(tidyverse)
+  library(knitr)
+
+# Custom function
+  fctr = function(...) {
+    args = rlang::list2(...)
+    rhs = map(args, rlang::f_rhs)
+    cases = case_when( !!!args )
+    exec(fct_relevel, cases, !!!rhs)
+  }  
+
+# Data
+  df <- read_csv('water.csv')
+
+  
+# Get the data you want ----------
+# Select variables
+  df2 <-
+    df %>%
+    select(
+      hh_id, age,   # items I want
+      hv201:hv215,  # range of vars I want
+      -hv213.       # don't want this
+    )
+  
+# Select cases (to keep)
+  df3 <-
+    df %>%
+    filter(
+      Q100 == 1,  # males only
+      Q1   <= 30  # check: is this a good idea?
+    ) 
+  
+    rm(df2, df3)
+    
+# mutate basics ------------------  
+# Create columns
+  df2 <-
+    df %>%
+    mutate(
+      rural = if_else(hv025 == 'rural', 1, 0),
+      age_months = age * 12,
+      age_dev = age - median(age, na.rm = T)
+    )
+    
+# Place the columns
+  df2 <- 
+    df %>%
+    mutate(
+      r_id = row_number(),
+      int_year = hv007,
+      .before = 1 # use column number or name; also .after
+    )
+  
+# Decide what to keep
+  df2 <-
+    df %>%
+    mutate(
+      r_id = row_number(),
+      age_yrs = age * 12,
+      .keep = 'none' # alts: 'used', 'unused'
+    )
+
+  
+# NAs and Strings ----------------
+# unwanted strings
+  df2 <-
+    df %>%
+    mutate(
+      watEx1 = as.numeric(water_mins), # changes strings to NA
+      waterFetchMins = case_when(
+        water_mins == 'on premises' ~ 0,
+        water_mins == '999' ~ NA, 
+        TRUE ~ as.numeric(water_mins)
+      ),
+      .keep = 'used'
+    )  # review the Warning messages; check: summary(df2$waterFetchMins)
+
+# Combine or split strings
+  df2 <-
+    df %>%
+    mutate(
+      r_id = row_number(),
+      region_x_rid = paste(region_id, r_id, sep = '_'),
+      region2 = str_extract(region_x_rid, "[^_]+"),
+      rid2 = str_extract(region_x_rid, "[^_]*$"),
+      .keep = 'used'
+    )
+
+# Change values to/from missing
+  df2 <-
+    df %>%
+    mutate(
+      attend1 = na_if(hv121, '9'),
+      attend2 = if_else(hv121 == '9', NA, hv121),
+      attend3 = if_else(is.na(attend2), 'dk/nr', attend2),
+      .keep = 'used'
+    )
+  
+# Factors and labels -------------  
+# Relevel
+  df2 <-
+    df %>%
+    mutate(
+      inc_quint = fct_relevel(
+        hh_income, # source variable
+        'poorest','poorer', 'middle','richer','richest'
+      )
+    )
+  
+# create ordered categories
+  df2 <-
+    df %>%
+    mutate(
+      ageFact = fctr(
+        age <= 9 ~ '6-9 years',
+        age %in% 10:12 ~ '10-12',
+        age >= 13 ~ '13-15' # vs TRUE
+      ),
+      .keep = 'used'
+    )
+  
+# Repeat across columns ----------  
+# Mutate across
+  df2 <-
+    df %>%
+    mutate(
+      across(where(is.character), ~na_if(.x, '9'))
+    )
+  
+# Grouping -----------------------
+# Aggregate: group and summarize
+  df2 <-
+    df %>%
+    group_by(hh_income) %>%
+    summarize(
+      mean_edu = mean(eduyrs, na.rm = T)
+    )
+  
+# Group level stats: group and mutate
+  df2 <-
+    df %>%
+    group_by(hh_income) %>%
+    mutate(
+      mean_edu = mean(eduyrs, na.rm = T)
+    ) %>%
+    ungroup()
+  
